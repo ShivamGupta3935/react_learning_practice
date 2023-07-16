79df90381b765664b3586fb0cfafadb0bb739c41
shivam gupta <guptashivam 3935@gmail.com>
2023-07-16 22:38:16 +0530

    Adding a button for event handling

--- src/components/subscriptionss/Subscription.js
+++ src/components/subscriptionss/Subscription.js
@@ -2,12 +2,17 @@ import Container from '../templates/Container';
 import './Subscription.css'
 import SubscriptionDate from './SubscriptionDate';
 const Subscription = (props) => {   
-   
+   let title = props.title;
+   const onButtonClicked= () => {
+    title = "change title";
+    console.log("button clicked");
+   }
     return(
         <Container className="subscription">
             <SubscriptionDate date={props.date}/>
-            <h2>{props.title}</h2>
+            <h2>{title}</h2>
             <div className="subscription_price">{props.amount}</div>
+            <button onClick={onButtonClicked}>change title</button>
         </Container>
     )
 }
